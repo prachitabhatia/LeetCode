2b79082e1c78895338e2e29aa3ec8d8d1e85640f
Prachita Bhatia <prachitamb@gmail.com>
2026-08-20 22:03:18 +0530

    Time: 166 ms (5.1%), Space: 148 MB (16.97%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        long long prefixSum = 0;
+        map<int,int> hash;
+        long long rem;
+        bool goodSubarray = false;
+        hash[0] = -1; //if the first two elements itself satisfy the condition then remainder = 0 needs to exist and since we are storing the value as index of the remainder, we can set the index of 0 as -1.
+
+        for(int i = 0; i < nums.size(); i++){
+            if(i==0){
+                prefixSum = nums[i];
+            }
+            else{
+                prefixSum += nums[i];
+            }
+
+            rem = ((prefixSum % k) + k) % k;
+            if (hash.find(rem) != hash.end()){
+                if(i - hash[rem] >= 2){
+                    goodSubarray = true;
+                    break;
+                }
+            }
+            else{
+                hash[rem] = i;
+            }
+        }
+        return goodSubarray;
+    }
+};

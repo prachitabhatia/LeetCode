cb502dd8eef1018c1b1b3a0827dbfde72be5be5b
Prachita Bhatia <prachitamb@gmail.com>
2026-09-18 16:45:22 +0530

    Time: 0 ms (100%), Space: 42.3 MB (16.7%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -6,30 +6,20 @@ public:
         vector<int> suffix(n);
         vector<int> product(n);
 
+        prefix[0] = 1;
+        suffix[n-1] = 1;
+
         //forming prefix array
-        for(int i = 0; i < n; i++){
-            if(i == 0){
-                prefix[i] = 1;
-            }
-
-            else if(i > 0){
-                prefix[i] = nums[i-1] * prefix[i-1];
-            }            
+        for(int i = 1; i < n; i++){
+            prefix[i] = nums[i-1] * prefix[i-1];         
         }
 
-         //forming suffix array
-        for(int i = n - 1; i >= 0; i--){
-            if(i == n - 1){
-                suffix[i] = 1;
-            }
-
-            else{
-                suffix[i] = suffix[i + 1] * nums[i + 1];
-            }            
-        }
+        //forming suffix array
+        for(int i = n - 2; i >= 0; i--){
+            suffix[i] = suffix[i + 1] * nums[i + 1];
+          }
 
         //product array = prefix array * suffix array
-
         for(int i = 0; i < n; i++){
             product[i] = prefix[i] * suffix[i];
         }
